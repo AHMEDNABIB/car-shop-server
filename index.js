0bcc8578a6764d616654598470be5a647175c91e
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-15 23:11:48 +0600

    get request for  single data complete

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
-
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
 
-
 // mongodb
 
-
-
-const uri =
-	`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dlgosdc.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dlgosdc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -35,7 +28,30 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
+		const serviceCollection = client.db("carDoctor").collection("services");
+
+		// get all services
+		app.get("/services", async (req, res) => {
+			const cursor = serviceCollection.find();
+			const result = await cursor.toArray();
+			res.send(result);
+		});
+
+		// get a single services with _id
+
+		app.get("/services/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+
+			const options = {
+				projection: { title: 1, price: 1, service_id: 1 },
+			};
+			const result = await serviceCollection.findOne(query, options);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
+
 		await client.db("admin").command({ ping: 1 });
 		console.log(
 			"Pinged your deployment. You successfully connected to MongoDB!"
@@ -47,11 +63,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
 	res.send("doctor is running");
 });
 
 app.listen(port, () => {
-    console.log(`Car Shop is running on port ${port}`)
-})
+	console.log(`Car Shop is running on port ${port}`);
+});
